7c5c464db10846f7cc9207038fa94398b592485d
jk.lee <jk.lee@m7system.com>
2013-03-15 09:27:34 +0900

    init

--- gen/com/google/api/services/samples/calendar/android2/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.api.services.samples.calendar.android2;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
